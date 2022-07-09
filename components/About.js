7c77cccd652dff4e65a52214fface00a861b112a
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-07-09 22:49:05 +0700

    :white_check_mark:  success Pomodoro

--- components/About.js
+++ components/About.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function About() {
+function About() {
   return (
-    <div className='w-11/12 mx-auto mt-36 text-white p-5'>
-        <div>
-            <h1 className='text-xl sm:text-2xl font-medium'>
-                <span className="border-b-4 border-red-400">What </span>
-                is  pomodoro Technique?
-            </h1>
-            <p className='mt-5 tracking-wide opacity-70 text-lg'>
-                The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.
-                The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks.
-            </p>
-        </div>
-
+    <div className="w-11/12 mx-auto mt-36 text-white p-5">
+      <div>
+        <h1 className="text-xl sm:text-2xl font-medium">
+          <span className="border-b-4 border-red-400 ">What</span> is Pomodoro
+          Technique?
+        </h1>
+        <p className="mt-5 tracking-wide opacity-70 text-lg">
+          The Pomodoro Technique is created by Francesco Cirillo for a more
+          productive way to work and study. The technique uses a timer to break
+          down work into intervals, traditionally 25 minutes in length,
+          separated by short breaks. Each interval is known as a pomodoro, from
+          the Italian word for 'tomato', after the tomato-shaped kitchen timer
+          that Cirillo used as a university student.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
+
+export default React.memo(About);
--- components/Timer.js
+++ components/Timer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import { FiBellOff } from "react-icons/fi";
 
 export default function Timer({
     stage,
@@ -6,35 +7,52 @@ export default function Timer({
     getTickingTime,
     seconds,
     ticking,
-    setTicking,
+    startTimer,
+    isTimeUp,
+    muteAlarm,
+    reset,
 }) {
-    const options = ["Pomodoro", "Short Break", "Long Break"]
+    const options = ["Pomodoro", "Short Break", "Long Break"];
     return (
-        <div className='w-10/12 mx-auto pt-5 text-white flex flex-col justify-center items-center mt-10'>
-            <div className='flex gap-5 items-center'>
+        <div className="w-10/12 mx-auto pt-5 text-white flex flex-col justify-center items-center mt-10">
+            <div className="flex gap-5 items-center">
                 {options.map((option, index) => {
                     return (
-                        <h1 key={index}
-                            className={`${index === stage ? "bg-gray-500 bg-opacity-30" : ""
+                        <h1
+                            key={index}
+                            className={` ${index === stage ? "bg-gray-500 bg-opacity-30" : ""
                                 } p-1 cursor-pointer transition-all rounded`}
                             onClick={() => switchStage(index)}
                         >
                             {option}
                         </h1>
-                    )
+                    );
                 })}
             </div>
-            <div className='mt-10 mb-10'>
+            <div className="mt-10 mb-10">
                 <h1 className="text-8xl font-bold select-none m-0">
-                    {getTickingTime()}:{seconds.toString().padStart(2, '0')}
+                    {getTickingTime()}:{seconds.toString().padStart(2, "0")}
                 </h1>
             </div>
-            <button 
-                className='px-16 py-2 text-2xl rounded-md bg-white text-blue-500 uppercase font-bold'
-                onClick={() => setTicking((ticking) => !ticking)}
-            >
-                {ticking ? "Stop" : "Start"}
-            </button>
+            <div className="flex gap-2 items-center">
+                <button
+                    className="px-16 py-2 text-2xl rounded-md bg-white text-blue-500 uppercase font-bold"
+                    onClick={startTimer}
+                >
+                    {ticking ? "Stop" : "Start"}
+                </button>
+                {isTimeUp && (
+                    <FiBellOff
+                        className="text-3xl text-white cursor-pointer"
+                        onClick={muteAlarm}
+                    />
+                )}
+            </div>
+            {ticking && (
+                <button className="uppercase text-white underline mt-5" onClick={reset}>
+                    Reset
+                </button>
+            )}
         </div>
-    )
+    );
 }
